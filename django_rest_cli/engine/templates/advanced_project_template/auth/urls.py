6716a9938609bd99ab1ea7a521ae97ea2020623c
Nyior <nyior.clement@aun.edu.ng>
2022-04-10 09:16:36 +0100

    add auth endpoints and custome user model to advanced template

---
+++ django_rest_cli/engine/templates/advanced_project_template/auth/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('account/', include('allauth.urls')),
+]
